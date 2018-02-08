d8ed58b49d81e276a1134a1060ce7422a1a04e6f
Ryan Lee Clark <rglc12@gmail.com>
2018-02-08 15:28:54 +0000

    Updates

--- worker.sh
+++ worker.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
 #install all required modules
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -10,12 +13,13 @@ echo "Git has been Installed!"
 
 git clone https://github.com/portsoc/clocoss-master-worker.git
 cd clocoss-master-worker
+sudo npm install
 
 echo "Master Worker Cloned and Installed!"
 
 #curl Key and IP address for each Server
-key=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/key" -H "Metadata-flavor: Google")
-ip=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/ip" -H "Metadata-flavor: Google")
+key=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/key" -H "Metadata-flav$
+ip=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/ip" -H "Metadata-flavor$
 
 #Run Client
 npm run client $key $ip:8080
