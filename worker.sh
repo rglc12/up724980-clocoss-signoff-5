559b0239f5865bf032c6a210e8c28e56efd2167d
Ryan Lee Clark <rglc12@gmail.com>
2018-02-08 15:58:04 +0000

    Updates

--- worker.sh
+++ worker.sh
@@ -18,8 +18,8 @@ sudo npm install
 echo "Master Worker Cloned and Installed!"
 
 #curl Key and IP address for each Server
-key=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/key" -H "Metadata-flav$
-ip=$(curl "metadata.google.internal/computeMetadata/v1/instance/attribute/ip" -H "Metadata-flavor$
+key=$(curl "metadata.google.internal/computeMetadata/v1/instance/attributes/key" -H "Metadata-flavor: Google")
+ip=$(curl "metadata.google.internal/computeMetadata/v1/instance/attributes/ip" -H "Metadata-flavor: Google")
 
 #Run Client
 npm run client $key $ip:8080
